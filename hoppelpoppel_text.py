b83d6ffd4957375ab12fcc95a1cdd5b782fdb343
DJ <djatschool7@gmail.com>
2021-03-30 15:49:41 +0200

    save

--- hoppelpoppel_text.py
+++ hoppelpoppel_text.py
@@ -123,7 +123,33 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
 
+# Creating a dataframe with German words in lowercase for count of unique values
 
+df2 = pd.Series(df.text3_lowercase.unique())
+df2 = pd.DataFrame(df2)
+
+df2.columns = ['text_unique']
+df2['count_text'] = ''
+
+# Counting the unique words
+
+for i in range(len(df2)):
+    x = df2.text_unique[i]      # x stores each single word in text_unique
+    df2['count_text'][i] = df[df.text3_lowercase == x].text3_lowercase.count()
+
+df['count_text'] = ''
+df['lat'] = ''
+df['lon'] = ''
+df['path_ID'] = ''
+df['path_start_end'] = ''
+
+for i in range(len(df2)):
+    x = df2.text_unique[i]
+    for j in range(len(df)):
+        if df.text3_lowercase[j] == x:
+            df['count_text'][j] = df2.count_text[i] 
+
+df.to_csv('fallada_hoppelpoppel.csv')
 
 
 
