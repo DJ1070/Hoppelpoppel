9678157c03a76f343758f50003adea16e6a496fc
DJ <djatschool7@gmail.com>
2021-03-30 15:45:58 +0200

    save

--- hoppelpoppel_text.py
+++ hoppelpoppel_text.py
@@ -46,7 +46,58 @@ except:
 
 words = list(chain.from_iterable(words))
 
+for i in range(len(words)):
+        words[i] = words[i].strip()
+        words[i] = words[i].replace('\xa0...', '')
+        words[i] = words[i].replace('<a', '')
+        words[i] = words[i].replace('</a>', '')
+        words[i] = words[i].replace('\n', '')
+        words[i] = words[i].replace('-', '')
+        words[i] = words[i].replace('–', '')
+        words[i] = words[i].replace('.', '')
+        words[i] = words[i].replace(':', '')
+        words[i] = words[i].replace(',', '')
+        words[i] = words[i].replace('>', '')
+        words[i] = words[i].replace('<', '')
+        words[i] = words[i].replace('›', '')
+        words[i] = words[i].replace('‹', '')
+        words[i] = words[i].replace('?', '')
+        words[i] = words[i].replace('!', '')
+        words[i] = words[i].replace('«', '')
+        words[i] = words[i].replace('»', '')
+        words[i] = words[i].replace('(', '')
+        words[i] = words[i].replace(')', '')
+        words[i] = words[i].replace(' ', '')
+        words[i] = re.sub(str('id='+'"'+'page\d'+'"'), '', words[i])
+        words[i] = re.sub(str('name='+'"'+'page\d'+'"'), '', words[i])
 
+        # Remove empty strings in words
+
+while("" in words) :
+    words.remove("")
+
+df = pd.DataFrame(words)
+
+df.reset_index(inplace = True)
+
+df.columns = ['rank', 'text']
+df['text2'] = ''
+
+# Cleaning last HTML code
+
+part1 = df[df.text == 'title="Dieter7/lac"']
+part2 = df[df.text == 'title="suru/Dieter7"']
+part3 = df[df.text == 'title="lac/Dieter7"']
+eraser = pd.concat([part1, part2, part3])
+
+erank = []
+for i in range(len(eraser)):
+    erank.append(int(eraser.index[i]))
+
+for i in erank:
+    df = df.drop([i])
+
+    
 
 
 
